1801b6a472c81f2e5d31acd9a54a32e4a3825499
SoufianKm <soufian.kamma@gmail.com>
2024-07-09 16:01:37 +0000

    Ordered listing

--- markdown2html.py
+++ markdown2html.py
@@ -17,30 +17,52 @@ if __name__ == '__main__':
     else:
         with open(sys.argv[1], "r") as r:
             with open(sys.argv[2], "w") as w:
-                start_ul = False
+                start_ul, start_ol = False, False
                 for line in r:
                     length = len(line)
                     heading_content = line.lstrip('#')
                     heading_nbr = length - len(heading_content)
                     unordered_content = line.lstrip('-')
                     unordered_nbr = length - len(unordered_content)
+                    ordered_content = line.lstrip('*')
+                    ordered_nbr = length - len(ordered_content)
                     # headings
                     if heading_nbr in range(1, 6):
                         html_line = '<h{}>{}</h{}>\n'.format(
-                                heading_nbr, heading_content.strip(), heading_nbr)
+                                heading_nbr, heading_content.strip(),
+                                heading_nbr)
                         w.write(html_line)
-                    
+
                     # Unordered Listing
                     if unordered_nbr:
                         if not start_ul:
                             w.write('<ul>\n')
                             start_ul = True
-                        html_line = '<li>{}<li>\n'.format(unordered_content.strip())
+                        html_line = '<li>{}<li>\n'.format(
+                                unordered_content.strip())
                         w.write(html_line)
-                    if start_ul and  not unordered_nbr:
+                    if start_ul and not unordered_nbr:
                         w.write('</ul>\n')
                         start_ul = False
+
+                    # Ordered Listing
+                    if ordered_nbr:
+                        if not start_ol:
+                            w.write('<ol>\n')
+                            start_ol = True
+                        html_line = '<li>{}<li>\n'.format(
+                                ordered_content.strip())
+                        w.write(html_line)
+                    if start_ol and not ordered_nbr:
+                        w.write('</ol>\n')
+                        start_ol = False
+
+                # close of Unordered Listing
                 if start_ul:
                     w.write('</ul>\n')
+
+                # close of Ordered Listing
+                if start_ol:
+                    w.write('</ol>\n')
 
         exit(0)
