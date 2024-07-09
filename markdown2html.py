b03d912194fb5bdfbbe5d8e4941358f543af4459
SoufianKm <soufian.kamma@gmail.com>
2024-07-09 15:43:40 +0000

    Unordered listing

--- markdown2html.py
+++ markdown2html.py
@@ -10,20 +10,37 @@ if __name__ == '__main__':
               file=sys.stderr)
         exit(1)
 
+    """Check if markdown file exist"""
     if not os.path.isfile(sys.argv[1]):
         print("Missing {}".format(sys.argv[1]), file=sys.stderr)
         exit(1)
     else:
         with open(sys.argv[1], "r") as r:
             with open(sys.argv[2], "w") as w:
+                start_ul = False
                 for line in r:
                     length = len(line)
                     heading_content = line.lstrip('#')
                     heading_nbr = length - len(heading_content)
+                    unordered_content = line.lstrip('-')
+                    unordered_nbr = length - len(unordered_content)
                     # headings
                     if heading_nbr in range(1, 6):
-                        html_line = '<h{}>'.format(
-                                heading_nbr) + heading_content.strip() +
-                        '</h{}>\n'.format(heading_nbr)
+                        html_line = '<h{}>{}</h{}>\n'.format(
+                                heading_nbr, heading_content.strip(), heading_nbr)
                         w.write(html_line)
+                    
+                    # Unordered Listing
+                    if unordered_nbr:
+                        if not start_ul:
+                            w.write('<ul>\n')
+                            start_ul = True
+                        html_line = '<li>{}<li>\n'.format(unordered_content.strip())
+                        w.write(html_line)
+                    if start_ul and  not unordered_nbr:
+                        w.write('</ul>\n')
+                        start_ul = False
+                if start_ul:
+                    w.write('</ul>\n')
+
         exit(0)
